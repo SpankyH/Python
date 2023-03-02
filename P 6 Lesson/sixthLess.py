4b31ca001a805725724f164c2e80173d07b38c3c
SpankyH <punk.martyn@mail.ru>
2023-03-02 03:24:48 +0300

    6 lesson

---
+++ P 6 Lesson/sixthLess.py
@@ -0,0 +1,31 @@
+############ ВЛОЖЕННЫЕ ЦИКЛЫ ############
+
+# for ПЕРЕМЕННАЯ in СПИСОК:
+#     for 2ПЕРЕМЕННАЯ in 2СПИСОК:
+#         print(ПЕРЕМЕННАЯ,2ПЕРЕМЕННАЯ)
+
+
+
+############ ПРИМЕРЫ ############
+# a = [1,2,3]
+# b = [4,5,6]
+
+# for i in a:
+#     for j in b:
+#         print(i,j)
+####
+# for i in range(1,2):
+#     for j in range(1,11):
+#         print(i, 'X', j, '=', i*j)
+####
+# for i in range(1,3):
+#     for j in range(1,11):
+#         if i == j:
+#             continue
+#         print(i, 'X', j, '=', i*j)
+####
+# for i in range(1,3):
+#     for j in range(1,11):
+#         if i == j:
+#             break
+#         print(i, 'X', j, '=', i*j)
